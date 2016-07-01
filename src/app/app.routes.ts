683acebff8f424ff767b3c71a94cd9a9f29063fd
David Brooks <david.brooks@talis.com>
2016-07-01 11:16:54 +0100

    Reverts routing changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { provideRouter, RouterConfig } from '@angular/router';
 import { ListComponent } from './list-component';
 
 export const routes: RouterConfig = [
-  { path: '', component: ListComponent }
+  { path: '',      redirectTo: '/items' },
+  { path: 'items', component: ListComponent }
 ];
 
 export const APP_ROUTER_PROVIDERS = [provideRouter(routes)];
