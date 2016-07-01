e66d0d567b430fae9f1cac2712a3c4369b1aada6
David Brooks <david.brooks@talis.com>
2016-07-01 11:09:24 +0100

    Sorts out routing BS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,7 @@ import { provideRouter, RouterConfig } from '@angular/router';
 import { ListComponent } from './list-component';
 
 export const routes: RouterConfig = [
-  { path: '',      redirectTo: '/items' },
-  { path: 'items', component: ListComponent }
+  { path: '', component: ListComponent }
 ];
 
 export const APP_ROUTER_PROVIDERS = [provideRouter(routes)];
